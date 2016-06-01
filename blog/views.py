654968abfe9ebc10099961783dec48daa80e35a2
icnovaro <icnovaro@gmail.com>
2016-06-01 17:52:04 -0500

    add the bootstrap template

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 3
+    paginate_by = 5
     template_name = 'blog/post/list.html'
 
 def post_list(request):
