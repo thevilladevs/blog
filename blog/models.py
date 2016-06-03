f2d6f2995ed35658c26923166fbfa03949d740b0
icnovaro <icnovaro@gmail.com>
2016-06-03 11:42:28 -0500

    add comments functionality

--- blog/models.py
+++ blog/models.py
@@ -50,7 +50,7 @@ class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
-    
+
     class Meta:
         ordering = ('created',)
 
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -17,9 +17,27 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
+    #list of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        # a comment was posted
+        comment_form = CommentForm(data = request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'comment_form': comment_form})
 
 def post_share(request, post_id):
     # Retrieve post by id
